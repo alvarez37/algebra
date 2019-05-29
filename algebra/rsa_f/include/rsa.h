487fef53b9a2ed4305be29d69f1b4df6df41e948
jean carlos <31245828+alvarez37@users.noreply.github.com>
2019-05-28 20:46:59 -0500

    Update rsa.h

--- algebra/rsa_f/include/rsa.h
+++ algebra/rsa_f/include/rsa.h
@@ -6,11 +6,7 @@
 #include "string"
 #include <cstdlib>
 
-<<<<<<< HEAD
-=======
-////////////////////
->>>>>>> 278d348adbaf8ae67e52d10be1aa54e1910ff302
-
+//gitweb
 using namespace std;
 using namespace NTL;
 
